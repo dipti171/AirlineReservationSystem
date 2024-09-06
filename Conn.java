b387ca23f9cd3886d0533aad7134b332adfeade0
Dipti yadav <147567295+dipti171@users.noreply.github.com>
2024-09-06 22:18:01 +0530

    Add files via upload

---
+++ Conn.java
@@ -0,0 +1,24 @@
+
+package airlinemanagementsystem;
+import java.sql.*;
+
+public class Conn 
+{
+    
+    Connection c;
+    Statement s;
+    
+    public Conn()
+    {
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c=DriverManager.getConnection("jdbc:mysql:///airlinemanagementsystem", "root","She2201@");
+            s=c.createStatement();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
